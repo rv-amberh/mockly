3a239e063ff2729d283e9609a982af994ef4804f
Amber Hall <94423627+rv-amberh@users.noreply.github.com>
2023-04-17 15:54:26 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+  Particles.init({
+    selector: ".background",
+    connectParticles: false,
+  });
+};
+
+document.getElementById('submit').addEventListener('click', () => {
+    const username = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    document.getElementById('email').value = "";
+    document.getElementById('password').value ="";
+
+    console.log(username, password);
+
+    const mainContent = document.getElementById('mainContainer');
+    const logoutButton = document.getElementById('logoutContent');
+    const loadDataButton = document.getElementById('loadData');
+    
+    mainContent.classList.toggle('signedIn');
+    logoutButton.classList.toggle('signedIn');
+    loadDataButton.classList.toggle('signedIn');
+
+    document.getElementById('user').innerHTML = `Welcome ${username}!`
+})
+
+document.getElementById('return').addEventListener('click', () => {
+    const mainContent = document.getElementById('mainContainer');
+    const logoutButton = document.getElementById('logoutContent');
+    const loadDataButton = document.getElementById('loadData');
+
+    
+    mainContent.classList.toggle('signedIn');
+    logoutButton.classList.toggle('signedIn');
+    loadDataButton.classList.toggle('signedIn');
+
+})
