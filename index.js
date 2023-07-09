a02c5ad4ce9b96d5d873f4deb86cb9ce6476e590
Amber M. Hall <amberhall@Ambers-MacBook-Air.local>
2023-07-09 13:45:49 -0700

    new updates

--- index.js
+++ index.js
@@ -6,23 +6,30 @@ window.onload = function () {
 };
 
 document.getElementById('submit').addEventListener('click', () => {
+
     const username = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    document.getElementById('email').value = "";
-    document.getElementById('password').value ="";
+    if(username.length === 0 || password.length === 0) {
+      alert("Enter a mock username and password.");
+      return false;
+    } else {
+      document.getElementById('email').value = "";
+      document.getElementById('password').value ="";
 
-    console.log(username, password);
+      console.log(username, password);
 
-    const mainContent = document.getElementById('mainContainer');
-    const logoutButton = document.getElementById('logoutContent');
-    const loadDataButton = document.getElementById('loadData');
     
-    mainContent.classList.toggle('signedIn');
-    logoutButton.classList.toggle('signedIn');
-    loadDataButton.classList.toggle('signedIn');
+      const mainContent = document.getElementById('mainContainer');
+      const logoutButton = document.getElementById('logoutContent');
+      const loadDataButton = document.getElementById('loadData');
+    
+      mainContent.classList.toggle('signedIn');
+      logoutButton.classList.toggle('signedIn');
+      loadDataButton.classList.toggle('signedIn');
 
-    document.getElementById('user').innerHTML = `Welcome ${username}!`
+      document.getElementById('user').innerHTML = `Welcome ${username}!`
+    }
 })
 
 document.getElementById('return').addEventListener('click', () => {
